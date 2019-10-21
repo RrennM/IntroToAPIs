1cc4bf8770e9a3bf24c1ab819d7e2908edfeaab2
Tyson Hood <TysonHood@gmail.com>
2019-10-21 16:08:24 -0700

    First API Call

---
+++ firstApiCall.js
@@ -0,0 +1,7 @@
+let request = require('request');
+request('https://jsonplaceholder.typicode.com/users/1', function(error, response, body) {
+    if(!error && response.statusCode == 200) {
+        var parsedData = JSON.parse(body);
+        console.log(parsedData['address']['geo']['lng']);
+    }
+})
