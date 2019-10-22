6dc96e475cf834f75491f82b5e7a58667fbedf31
Tyson Hood <TysonHood@gmail.com>
2019-10-22 10:07:25 -0700

    ES6 version with promises

--- firstApiCall.js
+++ firstApiCall.js
@@ -1,8 +1,17 @@
+// const request = require('request');
+// request('https://jsonplaceholder.typicode.com/users/1', (error, response, body) => {
+//     if(!error && response.statusCode == 200) {
+//         const parsedData = JSON.parse(body);
+//         console.log(`${parsedData.name} lives in ${parsedData.address.city}`);
+//     }
+// })
 
-const request = require('request');
-request('https://jsonplaceholder.typicode.com/users/1', (error, response, body) => {
-    if(!error && response.statusCode == 200) {
+const rp = require('request-promise');
+rp('https://jsonplaceholder.typicode.com/users/1')
+    .then((body) => {
         const parsedData = JSON.parse(body);
         console.log(`${parsedData.name} lives in ${parsedData.address.city}`);
-    }
-})
+    })
+    .catch((err) => {
+        console.log('Error! ', err)
+    })
