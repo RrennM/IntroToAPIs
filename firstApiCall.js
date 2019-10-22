a15236a06f51c0e284f0bdce42cced2f0875c699
Tyson Hood <TysonHood@gmail.com>
2019-10-22 09:37:45 -0700

    ES6 function calls and template literals

--- firstApiCall.js
+++ firstApiCall.js
@@ -1,7 +1,8 @@
-let request = require('request');
-request('https://jsonplaceholder.typicode.com/users/1', function(error, response, body) {
+
+const request = require('request');
+request('https://jsonplaceholder.typicode.com/users/1', (error, response, body) => {
     if(!error && response.statusCode == 200) {
-        var parsedData = JSON.parse(body);
-        console.log(parsedData['address']['geo']['lng']);
+        const parsedData = JSON.parse(body);
+        console.log(`${parsedData.name} lives in ${parsedData.address.city}`);
     }
 })
